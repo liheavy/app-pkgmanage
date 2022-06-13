a1ecfb9e160c35ee627762c649f533d9a0bf9d57
liheavy <lihaiwei8@huawei.com>
2022-06-13 15:03:15 +0800

    add panel
    add panel

--- auto_setup.sh
+++ auto_setup.sh
@@ -3,5 +3,7 @@
 bash /etc/pkgship/auto_install_pkgship_requires.sh redis
 bash /etc/pkgship/auto_install_pkgship_requires.sh elasticsearch
 su -c "pkgshipd start" pkgshipuser
-su -c "pkgship init" pkgshipuser
 pkgship init
+su -c "pkgship-paneld start" pkgshipuser
+su -c "pkgship-panel &" pkgshipuser
+
