e0ff687cdcc9d9f20d3169537946ec459825c2ce
gongzt <gong_zhengtang@163.com>
2022-08-04 14:25:23 +0800

    修复定时任务时区问题，加入定时检测进程任务

--- auto_setup.sh
+++ auto_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+/usr/sbin/crond start
+echo "*/1 * * * * /bin/bash /etc/pkgship/service-monitor.sh" > /var/spool/cron/root
 bash /etc/pkgship/auto_install_pkgship_requires.sh redis
 bash /etc/pkgship/auto_install_pkgship_requires.sh elasticsearch
 su -c "pkgshipd start" pkgshipuser
 pkgship init
 su -c "pkgship-paneld start" pkgshipuser
 su -c "pkgship-panel &" pkgshipuser
-
